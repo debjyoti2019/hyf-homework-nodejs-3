c75d753b648e18a9affe660157137e2befd7db84
debjyoti2019 <db.debjyotibanerjee@gmail.com>
2020-03-24 01:51:09 +0530

    step 2 done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const data={
+    "1":"Debjyoti",
+    "2":"Harsh",
+    "3": "Manu"
+}
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', function (req, res) {
+  res.json(data);
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
