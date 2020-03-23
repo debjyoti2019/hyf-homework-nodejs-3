9783c4630a50dd20e3f52ba4249865606f105d2a
debjyoti2019 <db.debjyotibanerjee@gmail.com>
2020-03-24 02:27:42 +0530

    hdbcjsdc

--- src/index.js
+++ src/index.js
@@ -13,5 +13,8 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/users', function (req, res) {
   res.send([]);
 })
+app.get('/users/:id', function (req, res) {
+    res.status(200).json({id:0});
+  })
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
